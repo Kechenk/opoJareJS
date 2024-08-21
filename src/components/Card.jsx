8fae086b8618ecfbaa550ddb6842b3dd83b076a3
kechenk <rezhakechenk67@gmail.com>
2024-08-22 04:33:17 +0700

    Changing all Card Component with Grid

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,46 @@
-import { Card, CardContent, Container, Grid } from "@mui/material";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { Card, CardContent } from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#e0e0e0",
+  ...theme.typography.body2,
+  padding: theme.spacing(6),
+  textAlign: "center",
+  color: "#424242",
+  fontWeight: "bold",
+}));
 
 export default function () {
   return (
-    <>
-      <Card sx={{ mt: 2 }}>
-        <CardContent>
-          <Grid>wkwkwk</Grid>
-        </CardContent>
-      </Card>
-    </>
+    <Box sx={{ mt: 8 }}>
+        <Grid 
+        justifyContent="space-around"
+        container spacing={6} 
+        columnSpacing={{ sm:2 , md:4 }}
+        >
+          <Grid item md={2}>
+            <Item>ITEM 1</Item>
+          </Grid>
+          <Grid item md={6}>
+            <Item>ITEM 2</Item>
+          </Grid>
+          <Grid item md={4}>
+            <Item>ITEM 3</Item>
+          </Grid>
+          <Grid item md={6}>
+            <Item>ITEM 4</Item>
+          </Grid>
+          <Grid item md={4}>
+            <Item>ITEM 5</Item>
+          </Grid>
+          <Grid item md={2}>
+            <Item>ITEM 6</Item>
+          </Grid>
+        </Grid>
+    </Box>
   );
 }
