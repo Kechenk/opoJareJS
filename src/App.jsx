1439d52d7bd81c522f6cd1942d7dd76a34f40044
kechenk <rezhakechenk67@gmail.com>
2024-08-24 01:10:21 +0700

    add roboto font and Typography Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { Button, Container, Link } from "@mui/material";
+import { Button, Container, Link, Typography } from "@mui/material";
 import * as React from "react";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import { Outlet } from "react-router-dom";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 // import Router from "./main/router"
 
 function App() {
   return (
     <Container sx={{ p: 4 }}>
-      <Navbar />
-      <Outlet />
+      <Typography>
+        <Navbar />
+        <Outlet />
+      </Typography>
     </Container>
   );
 }
